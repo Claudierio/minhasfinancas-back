bc5765d412ebdcc02cc359fdcb0db65eadd14055
Claudierio Baltazar <claudierio@gmail.com>
2022-12-23 22:44:30 -0300

    primeiro commit

---
+++ src/main/java/com/baltz/minhasfinancas/model/entity/TipoLancamento.java
@@ -0,0 +1,8 @@
+package com.baltz.minhasfinancas.model.entity;
+
+public enum TipoLancamento {
+	
+	//No enum, como todos os tipo saos valores constantes, colocamos tudo em letras maiusculas
+	RECEITA,
+	DESPESA
+}
